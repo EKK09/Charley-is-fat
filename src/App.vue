dfabe6fd7d21b3df456d8c20373040be0512481f
wei <xe856423@gmail.com>
2021-06-21 17:43:35 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    id="q-app"
+    :class="`bg-${bgColor}`"
+  >
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      bgColor: 'secondary',
+    };
+  },
+};
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <Header />
+    <q-drawer
+      v-model="right"
+      side="right"
+    >
+      <div
+        class="bg-red"
+      >
+        test
+      </div>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import Header from 'src/components/Header.vue';
+
+export default {
+  components: { Header },
+  data() {
+    return {
+      right: false,
+    };
+  },
+};
+</script>
