90aa89125b488a7c6710d57beacb1a38a00a734f
wei <xe856423@gmail.com>
2021-06-21 17:44:42 +0800

    新增基礎按鈕樣式

---
+++ src/components/button/ButtonBase.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-btn
+    size="md"
+    padding="6px"
+    unelevated
+    class="btn-bg"
+  >
+    <q-icon
+      v-if="icon"
+      :name="icon"
+      size="20px"
+    />
+    <slot />
+  </q-btn>
+</template>
+<script>
+export default {
+  name: 'ButtonBase',
+  props: {
+    icon: {
+      type: String,
+      default: undefined,
+    },
+  },
+};
+</script>
