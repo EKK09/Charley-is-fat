02625d5d80e1f203bf78a7e752d79bc8e6457263
wei <xe856423@gmail.com>
2021-06-22 21:10:45 +0800

    新增 dashboard 共用按鈕

---
+++ src/components/dashboard/DashboardHeaderButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+    class="button-base"
+    :style="style"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashboardHeaderButton',
+  props: {
+    padding: {
+      type: String,
+      default: () => '',
+    },
+  },
+  computed: {
+    style() {
+      if (!this.padding) {
+        return null;
+      }
+      return `padding: ${this.padding}`;
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.button-base {
+  display: inline-block;
+  white-space: nowrap;
+  height: 32px;
+  padding: 6px 5px;
+  color: white;
+  position: relative;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 3px;
+  cursor: pointer;
+  user-select: none;
+}
+
+.button-base:hover {
+  background-color: rgba(255, 255, 255, 0.3);
+}
+</style>
