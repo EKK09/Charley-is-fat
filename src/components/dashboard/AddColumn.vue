ae75944474a2c5d8e83e9d239ac85a6999f3bdf2
wei <xe856423@gmail.com>
2021-06-24 17:57:21 +0800

    新增建立列表元件

---
+++ src/components/dashboard/AddColumn.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dash-column row items-center fz-md">
+    <q-icon
+      name="add"
+      size="20px"
+      style="margin-right: 2px"
+    />
+    新增其他列表
+  </div>
+</template>
+<script>
+export default {
+  name: 'AddColumn',
+};
+</script>
+<style lang="scss" scoped>
+.dash-column {
+  width: 272px;
+  margin: 0 4px;
+  white-space: nowrap;
+  background-color: hsla(0,0%,100%,.24);
+  color: white;
+  cursor: pointer;
+  border-radius: 3px;
+  height: 40px;
+  min-height: 32px;
+  padding: 10px 12px;
+  transition: background 85ms ease-in,opacity 40ms ease-in,border-color 85ms ease-in;
+  line-height: 20px;
+}
+.dash-column:hover {
+   background-color: hsla(0,0%,100%,.32);
+}
+</style>
