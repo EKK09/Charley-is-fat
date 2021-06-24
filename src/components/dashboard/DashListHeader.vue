7f8ce9cfd3d066b150bd78ca91a21ded8b1625a3
wei <xe856423@gmail.com>
2021-06-24 17:56:31 +0800

    新增列表 header

---
+++ src/components/dashboard/DashListHeader.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="dash-list-header relative-position">
+    <textarea
+      ref="textarea"
+      v-model="title"
+      rows="1"
+      class="editable title-input cursor-pointer"
+      @input="handleTextareaInput"
+      @focus="handleTextareaFocus"
+    />
+    <div class="dash-list-header-icon flex flex-center cursor-pointer">
+      <q-icon
+        name="more_horiz"
+        size="20px"
+        style="color: #6b778c"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashListHeader',
+  data() {
+    return {
+      title: '完成事項',
+    };
+  },
+  methods: {
+    handleTextareaInput() {
+      setTimeout(() => {
+        const { textarea } = this.$refs;
+        textarea.style.height = 'auto';
+        const textareaHeight = textarea.scrollHeight;
+        textarea.style.height = `${textareaHeight}px`;
+      }, 0);
+    },
+    handleTextareaFocus() {
+      this.$refs.textarea.select();
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.dash-list-header {
+  padding: 6px 36px 0px 8px;
+}
+.title-input {
+  min-height: 20px;
+  max-height: 256px;
+  padding: 4px 8px;
+}
+.dash-list-header-icon {
+  position: absolute;
+  width: 32px;
+  height: 32px;
+  right: 4px;
+  top: 4px;
+  z-index: 1;
+  border-radius: 3px;
+
+  &:hover {
+    background-color: rgba(9,30,66,.08);
+  }
+}
+.editable:focus {
+  box-shadow: inset 0 0 0 2px #0079bf;
+  background-color: white;
+}
+</style>
