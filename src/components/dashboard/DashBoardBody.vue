eaf5589a4e28a96f87fda18083a5840aadc7ca4c
wei <xe856423@gmail.com>
2021-06-25 17:01:58 +0800

    修改列表卷軸樣式

--- src/components/dashboard/DashBoardBody.vue
+++ src/components/dashboard/DashBoardBody.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute fit row no-wrap list-wrapper">
+  <div class="row no-wrap list-wrapper">
     <DashList
       v-for="(column, index) in columns"
       :key="index"
@@ -27,6 +27,55 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+
+.list-wrapper {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  user-select: none;
+  white-space: nowrap;
+  margin-bottom: 8px;
+  overflow-x: auto;
+  overflow-y: hidden;
+  padding-bottom: 8px;
+
+  &::-webkit-scrollbar {
+    height: 12px;
+    width: 12px;
+  }
+  &::-webkit-scrollbar-button {
+    background: transparent;
+    display: block;
+    height: 4px;
+    width: 4px;
+  }
+
+  &::-webkit-scrollbar-track-piece {
+   background: rgba(0,0,0,.15);
+    &:vertical:start {
+      border-radius: 4px 4px 0 0;
+    }
+    &:vertical:end {
+      border-radius: 0 0 4px 4px;
+    }
+    &:horizontal:start {
+      border-radius: 4px 0 0 4px;
+    }
+    &:horizontal:end {
+      border-radius: 0 4px 4px 0;
+    }
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: rgba(255,255,255,0.5);
+    border-radius: 4px;
+    display: block;
+    height: 48px;
+  }
+
+}
 .list-wrapper > div:first-child {
   margin-left: 8px;
 }
