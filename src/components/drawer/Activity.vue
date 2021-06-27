e3524c67abcd1c86d3182a5123f768eb641f0745
wei <xe856423@gmail.com>
2021-06-27 17:53:29 +0800

    新增活動紀錄元件

---
+++ src/components/drawer/Activity.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="row no-wrap wrapper">
+    <q-avatar
+      size="32px"
+      class="avatar"
+    >
+      <img
+        src="/images/user.jpeg"
+        style="object-fit: cover"
+      >
+    </q-avatar>
+    <div class="col content-wrapper">
+      <div class="content">
+        <span class="name">查理布朗</span> 已變更這個看板的背景
+      </div>
+
+      <div class="time text-weight-medium">
+        昨天晚上10點59分
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Activity',
+
+};
+</script>
+<style lang="scss" scoped>
+
+.wrapper {
+  color: #172b4d;
+  word-wrap: break-word;
+  min-height: 56px;
+}
+
+.avatar {
+  margin: 8px 8px 4px 0;
+}
+.content-wrapper {
+  padding: 8px 0;
+}
+
+.name {
+  font-weight: 700;
+  word-wrap: break-word;
+}
+
+.time {
+  color: #5e6c84;
+  font-size: 12px;
+  line-height: 20px;
+  font-weight: 500;
+}
+</style>
